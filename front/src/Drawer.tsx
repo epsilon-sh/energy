57ceccb2227dff55996cb748f0b815b61a76307e
Mikhail Timofeev <mikko.timofeev@gmail.com>
2025-05-09 00:45:40 +0300

    [WIP] Selecting consumption metering points from drawer

---
+++ front/src/Drawer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface DrawerProps {}
+
+const Drawer: React.FC<DrawerProps> = ({ }) => {
+  return <nav style={{backgroundColor: 'red'}}>click to load metering points</nav>
+}
+
+export default Drawer;
--- front/src/EnergyDashboard.tsx
+++ front/src/EnergyDashboard.tsx
@@ -8,6 +8,7 @@ import { Area, Bar, Line } from "recharts";
 import { toSeconds, parse as parseDuration } from "iso8601-duration";
 import { startOfWeek, endOfDay, startOfDay } from "date-fns";
 import ContractsForm from "./ContractsForm";
+import Drawer from "./Drawer";
 
 const defaults = {
   start: startOfWeek(new Date()).toISOString(),
@@ -330,6 +331,8 @@ const EnergyDashboard: React.FC = () => {
           onUpdateContracts={setCurrentContracts}
         />
       </div>
+
+      <Drawer />
 
       <EnergyChart
         resolution={query.resolution}
