f056d3ab5dea2d69422e71588d84eeeb29f121eb
Mikhail Timofeev <mikko.timofeev@gmail.com>
2025-05-09 18:30:24 +0300

    lock main content area on drawer open

--- front/src/components/ui/Workspace.tsx
+++ front/src/components/ui/Workspace.tsx
@@ -2,7 +2,6 @@ import { FC, ReactNode } from "react";
 import Controls from "./Controls";
 import Background from "./Background";
 import { hashRoutes } from "@/types";
-import Drawer from './Drawer';
 
 interface WorkspaceProps {
   className?: string;
@@ -14,20 +13,9 @@ interface WorkspaceProps {
 const Workspace: FC<WorkspaceProps> = ({ open, className = '', controls, children }) => {
   return <Background>
     <div className={`workspace${className ? ' ' + className : ''}`}>
-      <input
-        type="checkbox"
-        id="drawer-toggle"
-        className="drawer-toggle display-none"
-      />
       <Controls>
-        <label htmlFor="drawer-toggle" className="drawer-button">
-          <span />
-          <span />
-          <span />
-        </label>
         {controls}
       </Controls>
-      <Drawer trackerCheckboxId="drawer-toggle" />
       {open && children}
     </div>
   </Background>
