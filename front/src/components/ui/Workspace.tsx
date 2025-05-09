f549076c2e66f52fafa5c9db57fe22cb25cc54ed
Mikhail Timofeev <mikko.timofeev@gmail.com>
2025-05-09 17:26:20 +0300

    integrated drawer into components

--- front/src/components/ui/Workspace.tsx
+++ front/src/components/ui/Workspace.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from "react";
 import Controls from "./Controls";
 import Background from "./Background";
 import { hashRoutes } from "@/types";
+import Drawer from './Drawer';
 
 interface WorkspaceProps {
   className?: string;
@@ -13,9 +14,20 @@ interface WorkspaceProps {
 const Workspace: FC<WorkspaceProps> = ({ open, className = '', controls, children }) => {
   return <Background>
     <div className={`workspace${className ? ' ' + className : ''}`}>
+      <input
+        type="checkbox"
+        id="drawer-toggle"
+        className="drawer-toggle display-none"
+      />
       <Controls>
+        <label htmlFor="drawer-toggle" className="drawer-button">
+          <span />
+          <span />
+          <span />
+        </label>
         {controls}
       </Controls>
+      <Drawer trackerCheckboxId="drawer-toggle" />
       {open && children}
     </div>
   </Background>
