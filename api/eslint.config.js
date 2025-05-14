17011b3f3fac8ed44b1a74a16e1dccf55852ec44
Mikhail Timofeev <mikko.timofeev@gmail.com>
2025-05-14 21:26:54 +0300

    cleaned up according to comments

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,7 +1,14 @@
 import globals from 'globals'
+import js from '@eslint/js'
+import eslintConfigStandard from 'eslint-config-standard'
+import importPlugin from 'eslint-plugin-import'
+import nPlugin from 'eslint-plugin-n'
+import promisePlugin from 'eslint-plugin-promise'
 
 export default [
+  js.configs.recommended,
   {
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
@@ -11,14 +18,20 @@ export default [
         ...globals.node,
       },
     },
+    plugins: {
+      import: importPlugin,
+      n: nPlugin,
+      promise: promisePlugin,
+    },
     rules: {
+      ...eslintConfigStandard.rules,
       'multiline-ternary': 'off',
       'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
       'arrow-parens': ['warn', 'as-needed'],
       'comma-dangle': ['error', 'always-multiline'],
-      'curly': ['warn', 'multi-or-nest'],
-      'indent': ['warn', 2, { offsetTernaryExpressions: false }],
-      'semi': ['error', 'never'],
+      curly: ['warn', 'multi-or-nest'],
+      indent: ['warn', 2, { offsetTernaryExpressions: false }],
+      semi: ['error', 'never'],
     },
   },
 ]
