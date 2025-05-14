4135f0c9b1233379a50b2c8a0cfcffaefde53135
Mikhail Timofeev <mikko.timofeev@gmail.com>
2025-05-14 21:26:54 +0300

    cleaned up according to comments

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,23 +1,53 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import reactHooks from 'eslint-plugin-react-hooks';
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooks from 'eslint-plugin-react-hooks'
 
 export default [
+  // Base configuration and ignores
+  js.configs.recommended,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist'],
   },
+  // Config files TypeScript configuration (without project reference)
   {
+    files: ['**/*.config.ts'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: null, // Don't require tsconfig for config files
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  // Main TypeScript configuration for source files
+  {
+    files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
       globals: {
         ...globals.browser,
+        ...globals.es2020,
       },
       parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
       'react-refresh': reactRefresh,
       'react-hooks': reactHooks,
     },
@@ -32,4 +62,4 @@ export default [
       'semi': ['error', 'always'],
     },
   },
-];
+]
